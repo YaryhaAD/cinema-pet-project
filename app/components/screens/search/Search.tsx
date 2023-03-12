7f703a1e27af40d7cb5fb192072e207665976b62
Andrey Yaryha <andreyyaryha@MacBook-Air-Andrey.local>
2023-03-12 19:16:35 +0300

    created search screen

--- app/components/screens/search/Search.tsx
+++ app/components/screens/search/Search.tsx
@@ -1,11 +1,31 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
+import { Field, Heading, Layout, Loader, MovieCatalog } from '@/components/ui'
+
+import { ISearchFormData } from './search.interface'
+import { useSearch } from './useSearch'
+
 const Search: FC = () => {
+	const { searchTerm, movies, isLoading, control } = useSearch()
+
 	return (
-		<View>
-			<Text>Search</Text>
-		</View>
+		<Layout isHasPadding>
+			<Heading title='Search' />
+			<View className='mt-3'>
+				<Field<ISearchFormData>
+					placeholder='Type something...'
+					control={control}
+					name='searchTerm'
+					keyboardType='web-search'
+				/>
+			</View>
+			{!!searchTerm ? (
+				<View className='mt-3'>
+					{isLoading ? <Loader /> : <MovieCatalog title='' movies={movies} />}
+				</View>
+			) : null}
+		</Layout>
 	)
 }
 
